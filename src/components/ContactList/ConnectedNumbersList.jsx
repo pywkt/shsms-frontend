e5011c798a96578d13d4112dd9992b2b52005009
pywkt <pyewacket+github@gggmail.me>
2021-10-07 15:40:37 -0400

    refactored contacts page

---
+++ src/components/ContactList/ConnectedNumbersList.jsx
@@ -0,0 +1,82 @@
+import React, { useContext } from "react";
+import { formatPhoneNumber } from "react-phone-number-input";
+import { SettingsContext } from "../../context/settingsContext";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import useStyles from './styles';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import ReceivedMessageList from "./ReceivedMessageList";
+import { updateSettings } from "../../api/settings";
+import { useTheme } from '@material-ui/core'
+import { Draggable } from "react-beautiful-dnd";
+
+const ConnectedNumbersList = ({ contacts }) => {
+    const theme = useTheme();
+    const classes = useStyles();
+    const settingsContext = useContext(SettingsContext)
+
+    const handleOpenList = (listIndex) => {
+        const callUpdateSettings = async (data) => {
+            await updateSettings({
+                ...settingsContext.settings,
+                openLists: data
+            })
+
+            settingsContext.setSettings({ ...settingsContext.settings, openLists: data })
+        }
+
+        if (settingsContext.settings?.openLists?.indexOf(listIndex) === -1) {
+            callUpdateSettings([...settingsContext.settings?.openLists, listIndex])
+        } else {
+            const allOpenLists = settingsContext.settings?.openLists
+            const updatedListArray = allOpenLists.filter(item => item !== listIndex)
+            callUpdateSettings(updatedListArray)
+        }
+    }
+
+    const updateStyle = (isDragging) => ({
+        border: isDragging && `1px dashed ${theme.palette.secondary.dark}`,
+        padding: isDragging && theme.spacing(1)
+    })
+
+    return (
+        <>
+            {contacts && contacts.map((item, index) => (
+                <Draggable key={item?.[0]} draggableId={item[0]} index={index}>
+                    {(provided, snapshot) => (
+                        <div
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                        >
+                            <div key={item?.[0]} style={{
+                                ...updateStyle(snapshot.isDragging),
+                                background: theme.palette.background.default,
+                            }}>
+
+                                <ListItem disableGutters button onClick={() => handleOpenList(item?.[0])} className={classes.collapsablePanel}>
+                                    <ListItemText
+                                        primary={formatPhoneNumber(item?.[0])}
+                                        primaryTypographyProps={{ color: 'textPrimary', variant: 'body2' }}
+                                    />
+                                    {settingsContext.settings?.openLists?.includes(item[0]) ? <ExpandLess /> : <ExpandMore />}
+                                </ListItem>
+
+                                <Collapse in={settingsContext.settings?.openLists?.indexOf(item?.[0]) !== -1} timeout="auto" unmountOnExit>
+                                    <List key={index} dense disablePadding>
+                                        <ReceivedMessageList message={item} />
+                                    </List>
+                                </Collapse>
+                            </div>
+                        </div>
+                    )}
+                </Draggable>
+            ))}
+        </>
+    )
+}
+
+export default ConnectedNumbersList;
--- src/components/ContactList/index.jsx
+++ src/components/ContactList/index.jsx
@@ -1,59 +1,20 @@
-import React, { useState, useEffect, useCallback, useContext } from "react";
-import { Link } from '@reach/router';
-import formatDistanceStrict from 'date-fns/formatDistanceStrict';
-import { formatPhoneNumber } from "react-phone-number-input";
-import { SettingsContext } from "../../context/settingsContext";
+import React, { useState, useEffect, useCallback } from "react";
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import Avatar from '@material-ui/core/Avatar';
 import Fab from '@material-ui/core/Fab'
 import AddIcon from '@material-ui/icons/Add';
 import useStyles from './styles';
-import PullToRefresh from "react-simple-pull-to-refresh";
-import Collapse from '@material-ui/core/Collapse';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
 import NewMessageForm from "../NewMessageForm";
 import { getContacts } from '../../api/contacts';
-import { updateSettings } from "../../api/settings";
-import { groupArrayOfObjects, sortArrayOfObjects } from "../../helpers/sorting";
-import { useTheme } from '@material-ui/core'
-import {
-    DragDropContext,
-    Draggable,
-    Droppable,
-    DropResult
-} from "react-beautiful-dnd";
+import { groupArrayOfObjects } from "../../helpers/sorting";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import ConnectedNumbersList from "./ConnectedNumbersList";
 
 
 const ContactList = ({ socket, updateTitlebar, incomingMessageCallback }) => {
-    const theme = useTheme();
     const classes = useStyles();
-    const settingsContext = useContext(SettingsContext)
     const [contacts, setContacts] = useState([])
     const [newMessageOpen, setNewMessageOpen] = useState(false);
     const handleNewMessageDialog = () => setNewMessageOpen((prev) => !prev);
-
-    const handleOpenList = (listIndex) => {
-        const callUpdateSettings = async (data) => {
-            await updateSettings({
-                ...settingsContext.settings,
-                openLists: data
-            })
-
-            settingsContext.setSettings({ ...settingsContext.settings, openLists: data })
-        }
-
-        if (settingsContext.settings?.openLists?.indexOf(listIndex) === -1) {
-            callUpdateSettings([...settingsContext.settings?.openLists, listIndex])
-        } else {
-            const allOpenLists = settingsContext.settings?.openLists
-            const updatedListArray = allOpenLists.filter(item => item !== listIndex)
-            callUpdateSettings(updatedListArray)
-        }
-    }
 
     const updateCallback = useCallback(() => {
         updateTitlebar('Messages')
@@ -89,12 +50,12 @@ const ContactList = ({ socket, updateTitlebar, incomingMessageCallback }) => {
 
     useEffect(updateCallback, [])
 
-    const handleRefresh = async () => {
-        const contacts03 = await getContacts();
-        const groupedContacts = groupArrayOfObjects(contacts03, 'toPhoneNumber')
+    // const handleRefresh = async () => {
+    //     const contacts03 = await getContacts();
+    //     const groupedContacts = groupArrayOfObjects(contacts03, 'toPhoneNumber')
 
-        setContacts(Object.entries(groupedContacts))
-    }
+    //     setContacts(Object.entries(groupedContacts))
+    // }
 
     const reorder = (list, startIndex, endIndex) => {
         const result = Array.from(list);
@@ -126,11 +87,6 @@ const ContactList = ({ socket, updateTitlebar, incomingMessageCallback }) => {
         [contacts]
     );
 
-    const updateStyle = (isDragging) => ({
-        border: isDragging && `1px dashed ${theme.palette.secondary.dark}`,
-        padding: isDragging && theme.spacing(1)
-    })
-
     return (
         <>
             <DragDropContext
@@ -142,61 +98,7 @@ const ContactList = ({ socket, updateTitlebar, incomingMessageCallback }) => {
                     {(provided) => (
                         <div ref={provided.innerRef}>
                             <List disablePadding>
-                                {contacts && contacts.map((item, index) => (
-                                    <Draggable key={item?.[0]} draggableId={item[0]} index={index}>
-                                        {(provided, snapshot) => (
-                                            <div
-                                                ref={provided.innerRef}
-                                                {...provided.draggableProps}
-                                                {...provided.dragHandleProps}
-                                            >
-                                                <div key={item?.[0]} style={{
-                                                    ...updateStyle(snapshot.isDragging),
-                                                    background: theme.palette.background.default,
-                                                }}>
-
-                                                    <ListItem disableGutters button onClick={() => handleOpenList(item?.[0])} className={classes.collapsablePanel}>
-                                                        <ListItemText
-                                                            primary={formatPhoneNumber(item?.[0])}
-                                                            primaryTypographyProps={{ color: 'textPrimary', variant: 'body2' }}
-                                                        />
-                                                        {settingsContext.settings?.openLists?.includes(item[0]) ? <ExpandLess /> : <ExpandMore />}
-                                                    </ListItem>
-
-                                                    <Collapse in={settingsContext.settings?.openLists?.indexOf(item?.[0]) !== -1} timeout="auto" unmountOnExit>
-                                                        <List key={index} dense disablePadding>
-
-                                                            {/* number that sent the text */}
-                                                            {sortArrayOfObjects(item?.[1], 'lastMessageRecieved', true).map(itemContact => (
-                                                                <Link
-                                                                    key={itemContact?._id}
-                                                                    to={`/messages/${itemContact.toPhoneNumber}/${itemContact.phoneNumber}`}
-                                                                    state={{ toPhoneNumber: itemContact.toPhoneNumber, fromPhoneNumber: itemContact.phoneNumber }}
-                                                                    style={{ textDecoration: 'none' }}
-                                                                >
-                                                                    <ListItem divider className={classes.fromNumberListItem}>
-                                                                        <ListItemAvatar>
-                                                                            <Avatar className={classes.contactsListAvatar} />
-                                                                        </ListItemAvatar>
-                                                                        <ListItemText
-                                                                            primary={itemContact?.alias || formatPhoneNumber(itemContact?.phoneNumber)}
-                                                                            primaryTypographyProps={{ color: 'textSecondary', variant: 'h3' }}
-                                                                            secondary={formatDistanceStrict(new Date(itemContact?.lastMessageRecieved), new Date()) + ' ago'}
-                                                                            secondaryTypographyProps={{
-                                                                                color: 'textSecondary',
-                                                                                variant: 'caption'
-                                                                            }}
-                                                                        />
-                                                                    </ListItem>
-                                                                </Link>
-                                                            ))}
-                                                        </List>
-                                                    </Collapse>
-                                                </div>
-                                            </div>
-                                        )}
-                                    </Draggable>
-                                ))}
+                                <ConnectedNumbersList contacts={contacts} />
                             </List>
                             {provided.placeholder}
                         </div>
